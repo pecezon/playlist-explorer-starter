f852a4c19693c265c08f6de16060ab001d13cb8b
Diego Lopez <diegolopez@meta.com>
2025-06-04 11:23:22 -0700

    Created the main wireframes for the sections of the app

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,16 @@
 
+const modal = document.getElementById("event-modal");
+const span = document.getElementsByClassName("close")[0];
+
+function openModal(festival) {
+   modal.style.display = "block";
+}
+
+span.onclick = function() {
+   modal.style.display = "none";
+}
+window.onclick = function(event) {
+   if (event.target == modal) {
+      modal.style.display = "none";
+   }
+}
