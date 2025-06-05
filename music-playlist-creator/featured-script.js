d01be44f6df8258e2b8c2c3359e214180e1c5cff
Diego Lopez <diegolopez@meta.com>
2025-06-05 14:49:32 -0700

    Finished mvp with required features

---
+++ music-playlist-creator/featured-script.js
@@ -0,0 +1,62 @@
+import { fetchPlaylists, fetchSongs } from "./script.js";
+let featuredPlaylist, playlistsArray, songsArray;
+
+//Pick featured playlist function
+function pickFeaturedPlaylist() {
+  return playlistsArray[Math.floor(Math.random() * playlistsArray.length)];
+}
+
+//Load Featured Playlist Page
+async function loadFeaturedPlaylistPage() {
+  try {
+    songsArray = await fetchSongs();
+    playlistsArray = await fetchPlaylists();
+  } catch (error) {
+    console.error(error);
+  }
+
+  featuredPlaylist = pickFeaturedPlaylist();
+
+  const featuredPlaylistInfoContainer = document.getElementById(
+    "featured-playlist-description"
+  );
+  featuredPlaylistInfoContainer.innerHTML = `
+      <img src="${featuredPlaylist.playlist_art}" alt="Playlist Image">
+      <div>
+         <h1>${featuredPlaylist.playlist_name}</h1>
+         <h3>${featuredPlaylist.playlist_author}</h3>
+      </div>
+   `;
+
+  //Songs
+  const featuredSongsContainer = document.getElementById(
+    "featured-playlist-songs"
+  );
+
+  console.log("Container" + featuredSongsContainer);
+
+  // Reset the innerHTML
+  featuredSongsContainer.innerHTML = "";
+
+  //Display song by song
+  for (const songID of featuredPlaylist.songs) {
+    console.log("Featured Play Song " + songID);
+    let song = songsArray[songID - 1];
+    featuredSongsContainer.innerHTML += `
+            <div class="featured-playlist-song">
+                <div class="featured-playlist-song-info">
+                    <i class="fa-solid fa-music"></i>
+                    <img src=${song.song_art} alt=${song.song_name}>
+                    <div>
+                        <h3>${song.song_name}</h3>
+                        <p>${song.song_author}</p>
+                    </div>
+                </div>
+                <h4>${song.song_album}</h4>
+                <h4>${song.song_duration}</h4>
+            </div>
+         `;
+  }
+}
+
+loadFeaturedPlaylistPage();
--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,97 +1,102 @@
 let songsArray, playlistsArray;
 
 //Fetch songs
-async function fetchSongs() {
-   try{
-      const res = await fetch("./data/songs.json");
-      if(!res.ok){
-         throw new Error(res.status)
-      }
+export async function fetchSongs() {
+  try {
+    const res = await fetch("./data/songs.json");
+    if (!res.ok) {
+      throw new Error(res.status);
+    }
 
-      const jsonSongs = await res.json();
-      console.log("Songs: " + jsonSongs)
-      return jsonSongs;
-   }catch(e){
-      console.error(e.message)
-   }
+    const jsonSongs = await res.json();
+    console.log("Songs: " + jsonSongs);
+    return jsonSongs;
+  } catch (e) {
+    console.error(e.message);
+  }
 }
 
 //Fetch playlists
-async function fetchPlaylists() {
-   try{
-      const res = await fetch("./data/data.json");
-      if(!res.ok){
-         throw new Error(res.status)
-      }
+export async function fetchPlaylists() {
+  try {
+    const res = await fetch("./data/data.json");
+    if (!res.ok) {
+      throw new Error(res.status);
+    }
 
-      const jsonPlaylits = await res.json();
-      console.log("Playlists: " + jsonPlaylits);
-      return jsonPlaylits;
-   }catch(e){
-      console.error(e.message)
-   }
+    const jsonPlaylits = await res.json();
+    console.log("Playlists: " + jsonPlaylits);
+    return jsonPlaylits;
+  } catch (e) {
+    console.error(e.message);
+  }
 }
 
+//Starting flow to render page
 async function startFlow() {
-   try {
-      //Fetch songs and playlists from the json files
-      songsArray = await fetchSongs();
-      playlistsArray = await fetchPlaylists();
+  try {
+    //Fetch songs and playlists from the json files
+    songsArray = await fetchSongs();
+    playlistsArray = await fetchPlaylists();
 
-      //Render playlists boxes
-      const playlistsContainer = document.getElementById('playlist-cards');
-      for(const playlist of playlistsArray){
-         console.log(playlist)
+    //Render playlists boxes
+    const playlistsContainer = document.getElementById("playlist-cards");
+    for (const playlist of playlistsArray) {
+      console.log(playlist);
 
-         let playlistCard = document.createElement('div');
-         playlistCard.className = 'playlist-card';
-         playlistCard.onclick = () => openModal(playlist)
+      //Create playlist card
+      let playlistCard = document.createElement("div");
+      playlistCard.className = "playlist-card";
 
-         let playlistImg = document.createElement('img');
-         playlistImg.src = playlist.playlist_art;
-         playlistImg.alt = playlist.playlist_name;
-         playlistCard.appendChild(playlistImg)
+      //Create Image Section
+      let playlistImg = document.createElement("img");
+      playlistImg.src = playlist.playlist_art;
+      playlistImg.alt = playlist.playlist_name;
+      playlistCard.appendChild(playlistImg);
+      playlistImg.onclick = () => openModal(playlist);
 
-         let info = document.createElement('div');
-         info.className = 'info'
-         let playlistName = document.createElement('h3');
-         playlistName.textContent = playlist.playlist_name;
-         info.appendChild(playlistName);
-         let playlistAuthor = document.createElement('p');
-         playlistAuthor.textContent = playlist.playlist_author;
-         info.appendChild(playlistAuthor);
-         playlistCard.appendChild(info)
+      let info = document.createElement("div");
+      info.className = "info";
+      let playlistName = document.createElement("h3");
+      playlistName.textContent = playlist.playlist_name;
+      info.appendChild(playlistName);
+      let playlistAuthor = document.createElement("p");
+      playlistAuthor.textContent = playlist.playlist_author;
+      info.appendChild(playlistAuthor);
+      playlistCard.appendChild(info);
+      info.onclick = () => openModal(playlist);
 
-         let likeContainer = document.createElement('div');
-         likeContainer.className = 'like-container';
-         let likeButton = document.createElement('button')
-         let likeIcon = document.createElement('i');
-         likeIcon.className = 'fa-regular fa-heart'
-         likeButton.appendChild(likeIcon);
-         let likeCount = document.createElement('p');
-         likeCount.textContent = playlist.likes;
-         likeContainer.appendChild(likeButton);
-         likeContainer.appendChild(likeCount);
-         playlistCard.appendChild(likeContainer);
+      let likeContainer = document.createElement("div");
+      likeContainer.className = "like-container";
+      let likeButton = document.createElement("button");
+      let likeIcon = document.createElement("i");
+      likeIcon.className = "fa-regular fa-heart";
+      likeButton.appendChild(likeIcon);
+      let likeCount = document.createElement("p");
+      likeCount.textContent = playlist.likes;
+      likeButton.onclick = () =>
+        likePlaylist(playlist.playlistID, likeButton, likeIcon, likeCount);
+      likeContainer.appendChild(likeButton);
+      likeContainer.appendChild(likeCount);
+      playlistCard.appendChild(likeContainer);
 
-         playlistsContainer.appendChild(playlistCard);
-      }
-
-   } catch (error) {
-      console.error(error.message)
-   }
-   
+      playlistsContainer.appendChild(playlistCard);
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
-//Loads everithing on loading
+//Loads everything on loading
 startFlow();
 
 //Load Modal on opening
 const modal = document.getElementById("event-modal");
 
+//Open Modal given a playlist object
 function openModal(playlist) {
-   try {
-      modal.innerHTML = `
+  try {
+    modal.innerHTML = `
          <div class="modal-content">
                <span class="close">&times;</span>
                <section class="modal-playlist-header">
@@ -99,32 +104,51 @@ function openModal(playlist) {
                   <div class="modal-playlist-info">
                      <h2>${playlist.playlist_name}</h2>
                      <h3>${playlist.playlist_author}</h3>
+                     <button id="shuffle-btn">Shuffle</button>
                   </div>
                </section>
                <section id="modal-playlist-songs">
                   
                </section>
          </div>
-      `
+      `;
 
-      //Modal logic
-      const span = document.querySelector(".close");
+    //Give a onclick to the shuffle button
+    const shuffleButton = document.getElementById("shuffle-btn");
+    shuffleButton.onclick = () => shufflePlaylist(playlist);
 
-      span.onclick = function() {
-         modal.style.display = "none";
+    //Render songs
+    loadSongs(playlist);
+
+    //Modal closing logic
+    const span = document.querySelector(".close");
+
+    span.onclick = function () {
+      modal.style.display = "none";
+    };
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
       }
-      window.onclick = function(event) {
-         if (event.target == modal) {
-            modal.style.display = "none";
-         }
-      }
+    };
 
-      //Load and generate songs containers
-      const songsContainer = document.getElementById('modal-playlist-songs');
-      console.log(playlist.songs)
-      for(const songID of playlist.songs){
-         let song = songsArray[songID-1];
-         songsContainer.innerHTML += `
+    modal.style.display = "block";
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+//Load and generate songs containers
+function loadSongs(playlist) {
+  const songsContainer = document.getElementById("modal-playlist-songs");
+  console.log(playlist.songs);
+
+  // Reset the innerHTML
+  songsContainer.innerHTML = "";
+
+  for (const songID of playlist.songs) {
+    let song = songsArray[songID - 1];
+    songsContainer.innerHTML += `
             <div class="modal-playlist-song-card">
                <div class="song-info">
                   <img src=${song.song_art} alt=${song.song_name} class="song-image">
@@ -136,12 +160,60 @@ function openModal(playlist) {
                </div>
                <h4>${song.song_duration}</h4>
             </div>
-         `
-      }
+         `;
+  }
+}
 
-      modal.style.display = "block";
-   } catch (error) {
-      console.log("bruh")
-      console.error(error)
-   }
+//Shuffle an array function
+function shuffleArray(array) {
+  let currentIndex = array.length;
+
+  while (currentIndex != 0) {
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
+
+//Shuffle playlist function
+function shufflePlaylist(playlist) {
+  playlist.songs = shuffleArray(playlist.songs);
+  loadSongs(playlist);
+}
+
+//Like process
+async function likePlaylist(playlistID, buttonContainer, icon, likeCount) {
+  if (buttonContainer.classList.contains("liked")) {
+    //Logic for already liked element
+
+    //Remove the class for styling
+    buttonContainer.classList.remove("liked");
+    icon.classList.remove("fa-solid");
+    icon.classList.add("fa-regular");
+
+    //Subtract 1 to the like total
+    playlistsArray[playlistID - 1].likes--;
+    likeCount.textContent = playlistsArray[playlistID - 1].likes;
+  } else {
+    //Logic for unliked element
+
+    //Add liked class for styling
+    buttonContainer.classList.add("liked");
+    icon.classList.remove("fa-regular");
+    icon.classList.add("fa-solid");
+
+    //Add 1 to the like total
+    if (playlistsArray[playlistID - 1].likes > 0) {
+      playlistsArray[playlistID - 1].likes++;
+    }
+    likeCount.textContent = playlistsArray[playlistID - 1].likes;
+  }
+
+  //TODO: here I would code the logic of saving the updated playlists, but  browser logic doesnt let the user to modify the files contents (POST, PUT, PATCH)
 }
